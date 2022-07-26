b90ee0c4e2b01c16d848dfa3d388000bfa0ac9bc
christian.tavera <christian.tavera@grupoargos.com.mx>
2022-07-26 12:14:38 -0500

    20. Testing Spring Security with JUnit 5. Closes #4

---
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIIT.java
@@ -0,0 +1,61 @@
+package guru.sfg.brewery.web.controllers;
+
+import guru.sfg.brewery.repositories.BeerInventoryRepository;
+import guru.sfg.brewery.repositories.BeerRepository;
+import guru.sfg.brewery.repositories.CustomerRepository;
+import guru.sfg.brewery.services.BeerService;
+import guru.sfg.brewery.services.BreweryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest
+public class BeerControllerIIT {
+    
+    @Autowired
+    WebApplicationContext webApplicationContext;
+    
+    MockMvc mockMvc;
+    
+    @MockBean
+    BeerRepository beerRepository;
+    
+    @MockBean
+    BeerInventoryRepository beerInventoryRepository;
+
+    @MockBean
+    CustomerRepository customerRepository;
+    
+    @MockBean
+    BreweryService breweryService;
+    
+    @MockBean
+    BeerService beerService;
+
+    @BeforeEach
+    void findBeers() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @WithMockUser("spring") //Tells mock, that we are login as spring username
+    @Test
+    void name() throws Exception {
+        mockMvc.perform(get("/beers/find"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/findBeers"))
+                .andExpect(model().attributeExists("beer"));
+    }
+}
