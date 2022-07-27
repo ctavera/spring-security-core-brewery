7c01c6a1ecb5dda466d724288c4b992615cced11
christian.tavera <christian.tavera@grupoargos.com.mx>
2022-07-27 17:15:15 -0500

    29. User Details Service. Closes #10

--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIIT.java
@@ -6,14 +6,23 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest
 @Import(SecurityConfig.class)
 public class BeerControllerIIT extends BaseIT {
-    
-//    @WithMockUser("spring") //Tells mock, that we are login as spring username, could be any name, no validation
+
+    @Test
+    void initCreationForm() throws Exception {
+        mockMvc.perform(get("/beers/new").with(httpBasic("user", "password")))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/createBeer"))
+                .andExpect(model().attributeExists("beer"));
+    }
+
+    //    @WithMockUser("spring") //Tells mock, that we are login as spring username, could be any name, no validation
     @Test
     void findBeers() throws Exception {
         mockMvc.perform(get("/beers/find"))
@@ -23,7 +32,7 @@ public class BeerControllerIIT extends BaseIT {
     }
 
     @Test
-    void findBeersWithHttpBasic() throws Exception {
+    void findBeersWithAnonymous() throws Exception {
         mockMvc.perform(get("/beers/find").with(anonymous())) //this check the authenthication
                 .andExpect(status().isOk())
                 .andExpect(view().name("beers/findBeers"))
