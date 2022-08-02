ce881142e6e9047e83114e20c37832468fd698ac
christian.tavera <christian.tavera@grupoargos.com.mx>
2022-08-02 13:25:14 -0500

    62. Refactor JUnit 5 Tests. Closes #40

--- src/test/java/guru/sfg/brewery/web/controllers/IndexControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/IndexControllerIT.java
@@ -1,8 +1,14 @@
 package guru.sfg.brewery.web.controllers;
 
 import guru.sfg.brewery.config.SecurityConfig;
+import guru.sfg.brewery.repositories.BeerInventoryRepository;
+import guru.sfg.brewery.repositories.BeerRepository;
+import guru.sfg.brewery.repositories.CustomerRepository;
+import guru.sfg.brewery.services.BeerService;
+import guru.sfg.brewery.services.BreweryService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -11,6 +17,21 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest //Only scans the necessary for test web
 @Import(SecurityConfig.class) //Needs this import to check SecurityFilterChain on WebMvcTest
 public class IndexControllerIT extends BaseIT {
+
+    @MockBean
+    BeerRepository beerRepository;
+
+    @MockBean
+    BeerInventoryRepository beerInventoryRepository;
+
+    @MockBean
+    CustomerRepository customerRepository;
+
+    @MockBean
+    BreweryService breweryService;
+
+    @MockBean
+    BeerService beerService;
 
     @Test
     void testGetIndexSlash() throws Exception {
--- src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
@@ -1,8 +1,17 @@
 package guru.sfg.brewery.web.controllers.api;
 
+import guru.sfg.brewery.domain.Beer;
+import guru.sfg.brewery.repositories.BeerOrderRepository;
+import guru.sfg.brewery.repositories.BeerRepository;
 import guru.sfg.brewery.web.controllers.BaseIT;
+import guru.sfg.brewery.web.model.BeerStyleEnum;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Random;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
@@ -11,6 +20,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 public class BeerRestControllerIT extends BaseIT {
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Autowired
+    BeerOrderRepository beerOrderRepository;
 
 //    @Test
 //    void deleteBeerUrlParams() throws Exception {
@@ -48,35 +63,51 @@ public class BeerRestControllerIT extends BaseIT {
 //                .andExpect(status().isOk());
 //    }
 
-    @Test
-    void deleteBeerHttpBasic() throws Exception {
-        //Authentication Filter Use Case: legacy aplication who send headers with Api Key and Secret, not recommended
-        mockMvc.perform(delete("/api/v1/beer/cfca9075-1cc5-4532-84ea-739544af7144")
-                        .with(httpBasic("spring", "kahlua")))
-                .andExpect(status().is2xxSuccessful());
-    }
+    @DisplayName("Delete Tests")
+    @Nested
+    class DeleteTests{
 
-    @Test
-    void deleteBeerHttpBasicUserRole() throws Exception {
-        //Authentication Filter Use Case: legacy aplication who send headers with Api Key and Secret, not recommended
-        mockMvc.perform(delete("/api/v1/beer/cfca9075-1cc5-4532-84ea-739544af7144")
-                        .with(httpBasic("user", "password")))
-                .andExpect(status().isForbidden()); //not allowed to the resource
-    }
+        public Beer beerToDelete(){
+            Random random = new Random();
 
-    @Test
-    void deleteBeerHttpBasicCustomerRole() throws Exception {
-        //Authentication Filter Use Case: legacy aplication who send headers with Api Key and Secret, not recommended
-        mockMvc.perform(delete("/api/v1/beer/cfca9075-1cc5-4532-84ea-739544af7144")
-                        .with(httpBasic("scott", "tiger")))
-                .andExpect(status().isForbidden());
-    }
+            return beerRepository.saveAndFlush(Beer.builder()
+                    .beerName("Delete Me Beer")
+                    .beerStyle(BeerStyleEnum.IPA)
+                    .minOnHand(12)
+                    .quantityToBrew(200)
+                    .upc(String.valueOf(random.nextInt(99999999)))
+                    .build());
+        }
+        @Test
+        void deleteBeerHttpBasic() throws Exception {
+            //Authentication Filter Use Case: legacy aplication who send headers with Api Key and Secret, not recommended
+            mockMvc.perform(delete("/api/v1/beer/" + beerToDelete().getId())
+                            .with(httpBasic("spring", "kahlua")))
+                    .andExpect(status().is2xxSuccessful());
+        }
 
-    @Test
-    void deleteBeerNoAuth() throws Exception {
-        //Authentication Filter Use Case: legacy aplication who send headers with Api Key and Secret, not recommended
-        mockMvc.perform(delete("/api/v1/beer/cfca9075-1cc5-4532-84ea-739544af7144"))
-                .andExpect(status().isUnauthorized());
+        @Test
+        void deleteBeerHttpBasicUserRole() throws Exception {
+            //Authentication Filter Use Case: legacy aplication who send headers with Api Key and Secret, not recommended
+            mockMvc.perform(delete("/api/v1/beer/" + beerToDelete().getId())
+                            .with(httpBasic("user", "password")))
+                    .andExpect(status().isForbidden()); //not allowed to the resource
+        }
+
+        @Test
+        void deleteBeerHttpBasicCustomerRole() throws Exception {
+            //Authentication Filter Use Case: legacy aplication who send headers with Api Key and Secret, not recommended
+            mockMvc.perform(delete("/api/v1/beer/" + beerToDelete().getId())
+                            .with(httpBasic("scott", "tiger")))
+                    .andExpect(status().isForbidden());
+        }
+
+        @Test
+        void deleteBeerNoAuth() throws Exception {
+            //Authentication Filter Use Case: legacy aplication who send headers with Api Key and Secret, not recommended
+            mockMvc.perform(delete("/api/v1/beer/" + beerToDelete().getId()))
+                    .andExpect(status().isUnauthorized());
+        }
     }
 
     @Test
@@ -87,13 +118,22 @@ public class BeerRestControllerIT extends BaseIT {
 
     @Test
     void findBeerById() throws Exception {
-        mockMvc.perform(get("/api/v1/beer/cfca9075-1cc5-4532-84ea-739544af7144"))
+        Beer beer = beerRepository.findAll().get(0);
+
+        mockMvc.perform(get("/api/v1/beer/" + beer.getId()))
                 .andExpect(status().isOk());
     }
 
     @Test
     void findBeerByUpc() throws Exception {
         mockMvc.perform(get("/api/v1/beerUpc/0631234200036"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void findBeerFormADMIN() throws Exception {
+        mockMvc.perform(get("/beers").param("beerName", "")
+                        .with(httpBasic("spring", "kahlua")))
                 .andExpect(status().isOk());
     }
 }
