30a9419e251744bdb8334e4e117c5803789b522a
christian.tavera <christian.tavera@grupoargos.com.mx>
2022-08-03 11:04:19 -0500

    68. Assignment 9 - Define Roles for Customer Operations. Closes #47

--- src/main/java/guru/sfg/brewery/domain/security/Role.java
+++ src/main/java/guru/sfg/brewery/domain/security/Role.java
@@ -23,7 +23,7 @@ public class Role {
     private Set<User> users;
 
     @Singular //We can add in a Singular Authority via the Builder Pattern
-    @ManyToMany(cascade = {CascadeType.MERGE, CascadeType.PERSIST}, fetch = FetchType.EAGER) //Eager creates only one query to the database, instead of multiple in lazy
+    @ManyToMany(cascade = CascadeType.MERGE, fetch = FetchType.EAGER) //Eager creates only one query to the database, instead of multiple in lazy
     @JoinTable(name = "role_authority",
             joinColumns = {@JoinColumn(name = "ROLE_ID", referencedColumnName = "ID")},
             inverseJoinColumns = {@JoinColumn(name = "AUTHORITY_ID", referencedColumnName = "ID")})
