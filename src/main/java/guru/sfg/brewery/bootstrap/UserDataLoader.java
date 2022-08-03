eb39f7ecea217417e0ba5297868f581f607af9df
christian.tavera <christian.tavera@grupoargos.com.mx>
2022-08-03 10:07:50 -0500

    68. Defining Roles for Beer Operations. Closes #46

--- src/main/java/guru/sfg/brewery/bootstrap/UserDataLoader.java
+++ src/main/java/guru/sfg/brewery/bootstrap/UserDataLoader.java
@@ -1,7 +1,9 @@
 package guru.sfg.brewery.bootstrap;
 
+import guru.sfg.brewery.domain.security.Authority;
 import guru.sfg.brewery.domain.security.Role;
 import guru.sfg.brewery.domain.security.User;
+import guru.sfg.brewery.repositories.security.AuthorityRepository;
 import guru.sfg.brewery.repositories.security.RoleRepository;
 import guru.sfg.brewery.repositories.security.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +12,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.Set;
+
 @Slf4j
 @RequiredArgsConstructor
 @Component
@@ -17,18 +22,30 @@ public class UserDataLoader implements CommandLineRunner {
 
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final AuthorityRepository authorityRepository;
     private final PasswordEncoder passwordEncoder;
 
     private void loadSecurityData() {
+        //beer auths
+        Authority createBeer = authorityRepository.save(Authority.builder().permission("beer.create").build());
+        Authority updateBeer = authorityRepository.save(Authority.builder().permission("beer.update").build());
+        Authority readBeer = authorityRepository.save(Authority.builder().permission("beer.read").build());
+        Authority deleteBeer = authorityRepository.save(Authority.builder().permission("beer.delete").build());
 
-        Role admin = roleRepository.save(Role.builder().name("ROLE_ADMIN").build());
-        Role userRole = roleRepository.save(Role.builder().name("ROLE_USER").build());
-        Role customer = roleRepository.save(Role.builder().name("ROLE_CUSTOMER").build());
+        Role adminRole = roleRepository.save(Role.builder().name("ADMIN").build());
+        Role customerRole = roleRepository.save(Role.builder().name("CUSTOMER").build());
+        Role userRole = roleRepository.save(Role.builder().name("USER").build());
+
+        adminRole.setAuthorities(Set.of(createBeer, updateBeer, readBeer, deleteBeer));
+        customerRole.setAuthorities(Set.of(readBeer));
+        userRole.setAuthorities(Set.of(readBeer));
+
+        roleRepository.saveAll(Arrays.asList(adminRole, customerRole, userRole));
 
         userRepository.save(User.builder()
                 .username("spring")
                 .password(passwordEncoder.encode("kahlua"))
-                .role(admin)
+                .role(adminRole)
                 .build());
 
         userRepository.save(User.builder()
@@ -40,7 +57,7 @@ public class UserDataLoader implements CommandLineRunner {
         userRepository.save(User.builder()
                 .username("scott")
                 .password(passwordEncoder.encode("tiger"))
-                .role(customer)
+                .role(customerRole)
                 .build());
 
         log.debug("Users Loaded: " + userRepository.count());
